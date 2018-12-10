fd13af8163205c0a5fa9f7f8059d1ccdccb77bc7
pawvel <pawelrzesnicki@wp.pl>
2018-12-11 00:08:46 +0100

    Dokończone WORK oraz zrobiony pokaz slajdów

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var slideIndex = 1;
+
+function currentSlide(n) {
+  slideIndex = n;
+
+  var slide = document.getElementsByClassName("slide");
+  var dot = document.getElementsByClassName("dot");
+
+  for (i = 0; i < slide.length; i++) {
+    slide[i].style.display = "none";
+  }
+
+  for (i = 0; i < dot.length; i++) {
+    dot[i].className = dot[i].className.replace(" active", "");
+  }
+
+  slide[slideIndex - 1].style.display = "flex";
+  dot[slideIndex - 1].className += " active";
+}
